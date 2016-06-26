c175ad86ecb3814358fd72414eaaa3f9f009adc5
Dax <limeside@gmail.com>
2016-06-26 17:56:44 -0500

    Add bookshelf db configuration file, setting up knex migration for approval table. :rabbit:

---
+++ db/bookshelf.js
@@ -0,0 +1,8 @@
+var environment   = process.env.NODE_ENV || 'development',
+    configuration = require('../knexfile')[environment],
+    knex          = require('knex')(configuration),
+    bookshelf     = require('bookshelf')(knex);
+
+bookshelf.plugin('registry')
+
+module.exports = bookshelf;
