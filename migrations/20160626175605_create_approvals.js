cef731e8fd55a388b5ad0f4fc2021321bbe8d1cc
Dax <limeside@gmail.com>
2016-06-26 17:58:56 -0500

    Add migration for approved_advice table. :rabbit:

--- migrations/20160626175605_create_approvals.js
+++ migrations/20160626175605_create_approvals.js
@@ -1,8 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('approved_advice', table => {
+
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('approved_advice');
 };
