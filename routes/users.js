fa2b09fb87444acf1860de5be82864890c13dd29
Dax <limeside@gmail.com>
2016-06-27 09:52:00 -0500

    Edit user route boilerplates. :rabbit:

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,15 @@ router.route('/')
     res.send('Default users route.');
   });
 
+router.route('/:id/edit')
+  // Display edit page.
+  .get((req, res) => {
+
+  })
+
+  // Allow user to update profile information if not using Twitter/Facebook
+  .put((req, res) => {
+
+  });
+
 module.exports = router;
