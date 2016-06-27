fa402578c6bdb0dc85a1847c572f07995b760420
Dax <limeside@gmail.com>
2016-06-27 12:55:45 -0500

    Add migration to include password column in users table. :rabbit:

--- migrations/20160627125328_add_password.js
+++ migrations/20160627125328_add_password.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.table('users', table => {
+    table.text('password');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.table('users', table => {
+    table.dropColumn('password');
+  });
 };
