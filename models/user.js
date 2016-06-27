a425b3764e1eb63da5bd6212266170cb51ee6cc2
Dax <limeside@gmail.com>
2016-06-27 08:35:30 -0500

    Add user model.

--- models/user.js
+++ models/user.js
@@ -1,12 +1,16 @@
 var bookshelf = require('../db/bookshelf');
 
 require('./post');
+require('./approved_post');
 
 var User = bookshelf.Model.extend({
   table:'users',
   posts: function() {
     return this.hasMany('Post');
+  },
+  approved: function() {
+    return this.hasMany('Approved_Post');
   }
 });
 
-module.exports = ()
+module.exports = bookshelf.model('User', User);
