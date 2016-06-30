6ff375dd47809e18d6e047b7251a33905bb4b72c
Dax <limeside@gmail.com>
2016-06-30 16:36:45 -0500

    Add admin calls, working! :rabbit:

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,29 @@
 var express = require('express'),
-    router  = express.Router();
+    router  = express.Router(),
+    User    = require('../models/user'),
+    Post    = require('../models/post');
 
 router.route('/')
   // Display admin panel.
   .get((req, res) => {
+    User.fetchAll()
+      .then( users => {
+        var allUsers = users.toJSON();
+
+        Post.where({ approved: false })
+          .fetchAll()
+          .then( posts => {
+            allPosts = posts.toJSON();
+            eval(locus)
+            res.render('admin', {
+              title: 'goTravel -- Administrator Page',
+              layout: 'adminPage',
+              users: allUsers,
+              posts: allPosts
+            });
+          })
+      })
     // Display all posts, and related users.
-    res.render('admin', {
-        title: 'goTravel -- Administrator Page',
-        layout: 'adminPage'
-    });
     // Have the option to update/delete?
   })
 
