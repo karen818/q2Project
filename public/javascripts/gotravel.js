78f5b02a2b6de5737ffca165d4b7b0515b0c43de
Dax <limeside@gmail.com>
2016-07-01 09:18:22 -0500

    Add https in headers, fixed typo. :rabbit:

--- public/javascripts/gotravel.js
+++ public/javascripts/gotravel.js
@@ -37,7 +37,7 @@ $(document).ready(() => {
 
     ///////// Photo Ajax //////////
     $.ajax({
-        url: 'httpss://api.unsplash.com/photos/search?category=4&query=nature&client_id=b21a9e8e8640c016bfff18622954eaa29cb31d67178c80f5e36efeaa02e9cb70',
+        url: 'https://api.unsplash.com/photos/search?category=4&query=nature&client_id=b21a9e8e8640c016bfff18622954eaa29cb31d67178c80f5e36efeaa02e9cb70',
         type: "GET",
         dataType: 'json',
     }).done(function(data){
